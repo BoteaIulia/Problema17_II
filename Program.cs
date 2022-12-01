5b68b8a1895f57fe7ccae2c730023be4e165aec8
dynamics <31436988+dynamics1@users.noreply.github.com>
2022-12-01 23:41:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public static class Globals
+{
+	internal static int Main()
+
+	{
+		int n = 0;
+		scanf_s("%d", n);
+		int a;
+		int x = 0;
+		int maxv = 0;
+		for (int i = 0; i < n; i++)
+		{
+			scanf_s("%d", a);
+			if (x < 0)
+			{
+				return -1; //incorect
+			}
+			if (a == 0)
+			{
+				x++;
+			}
+			else
+			{
+				if (a == 1)
+				{
+					x--;
+				}
+			}
+			if (x > maxv)
+			{
+				maxv = x;
+			}
+		}
+		if (x != 0)
+		{
+			return -1;
+		}
+		return maxv;
+	}
+}
